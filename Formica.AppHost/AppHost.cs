4292a860713ee32f2c534494fd7578035e0661cb
Igor Mogilnitskiy <igmo71@outlook.com>
2026-05-12 18:03:32 +0300

    Implement Warehouse Foundation setup tasks

--- Formica.AppHost/AppHost.cs
+++ Formica.AppHost/AppHost.cs
@@ -1,8 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
+var postgres = builder.AddPostgres("postgres");
+var warehouseDatabase = postgres.AddDatabase("warehouse");
 
 var apiService = builder.AddProject<Projects.Formica_ApiService>("apiservice")
+    .WithReference(warehouseDatabase)
+    .WaitFor(warehouseDatabase)
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.Formica_Web>("webfrontend")
